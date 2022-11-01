c6e56f7728969f3cd356341f1093b8704f11f6f0
joacogambra <joaquingambra@gmail.com>
2022-11-01 11:02:21 -0300

    appjs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,54 @@
 import './style.css'
+import data from './data/events1'
+import NombreFoto from './components/NombreFoto';
+import CampoConArray from './components/CampoConArray';
+import Flechita from './components/Flechita';
+import { useState } from 'react';
 
 function App() {
+  let [mostrarOcultar, setMostrarOcultar] = useState(false)
+  let [numeroCambiar, setNumeroCambiar] = useState(0)
+
+  let hide = () =>{
+    setMostrarOcultar(!mostrarOcultar)
+    console.log(mostrarOcultar);
+  }
+
+  
+  let next = () =>{
+    if(numeroCambiar<data.length-1){
+      setNumeroCambiar(++numeroCambiar)
+    }else{
+      setNumeroCambiar(0)
+    }
+  }
+  let prev = () => {
+    if(numeroCambiar>0){
+      setNumeroCambiar(--numeroCambiar)
+    }else{
+      setNumeroCambiar(data.length-1)
+    }
+  }
+
   return (
-    <h1>Hello React</h1>
+    <div className='card'>
+      <NombreFoto category={data[numeroCambiar].category} foto={data[numeroCambiar].image} name={data[numeroCambiar].name}/>
+      <div className='ocultar-mostrar'>
+        {
+        mostrarOcultar ?
+        (<>
+        <Flechita  clases='arriba' onClick={hide}/>
+        <CampoConArray text={data[numeroCambiar].description}/>
+        </>) :
+        (<Flechita clases='abajo' onClick={hide}/>)
+        }
+      </div>     
+      
+      <div className='flechas'> 
+        <Flechita clases='izquierda' izquierda={prev}/>
+        <Flechita  clases='derecha' derecha={next}/>
+      </div> 
+    </div>
   );
 }
 
