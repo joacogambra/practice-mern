2a544d55b201ab15e22ab4f7be662f8a1a533ab5
joacogambra <joaquingambra@gmail.com>
2022-11-01 11:04:13 -0300

    campoconarray

---
+++ src/components/CampoConArray.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function CampoConArray(props) {
+    let text = props.text
+    return (
+    <div>
+        <p className='description'>Description : {text}</p>
+    </div>
+  )
+}
